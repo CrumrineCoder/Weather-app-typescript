0b6bec20ff19ef21969f72d2a976905219039fe4
Nicolas Crumrine <crumrinecoding@gmail.com>
2024-02-04 15:17:35 -0600

    Move code to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { fetchWeatherApi } from "openmeteo";
+const url = "https://api.open-meteo.com/v1/forecast";
+
+let latitude: number | undefined;
+let longitude: number | undefined;
+
+function handleLocation(position: GeolocationPosition) {
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+  if (latitude && longitude) {
+    getWeather(latitude, longitude);
+  }
+}
+
+navigator.geolocation.getCurrentPosition(handleLocation);
+
+async function getWeather(latitude: number, longitude: number) {
+  const params = {
+    latitude: latitude,
+    longitude: longitude,
+    hourly: "temperature_2m",
+  };
+
+  const responses = await fetchWeatherApi(url, params);
+
+  // Helper function to form time ranges
+  const range = (start: number, stop: number, step: number) =>
+    Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
+  // Process first location. Add a for-loop for multiple locations or weather models
+  const response = responses[0];
+
+  // Attributes for timezone and location
+  const utcOffsetSeconds = response.utcOffsetSeconds();
+  const timezone = response.timezone();
+  const timezoneAbbreviation = response.timezoneAbbreviation();
+  //const latitude = response.latitude();
+  //const longitude = response.longitude();
+
+  const hourly = response.hourly()!;
+
+  // Note: The order of weather variables in the URL query and the indices below need to match!
+  const weatherData = {
+    hourly: {
+      time: range(
+        Number(hourly.time()),
+        Number(hourly.timeEnd()),
+        hourly.interval()
+      ).map((t) => new Date((t + utcOffsetSeconds) * 1000)),
+      temperature2m: hourly.variables(0)!.valuesArray()!,
+    },
+  };
+
+  // `weatherData` now contains a simple structure with arrays for datetime and weather data
+  for (let i = 0; i < weatherData.hourly.time.length; i++) {
+    console.log(
+      "Hourly Time: " + weatherData.hourly.time[i].toISOString(),
+      "Temperature: " + weatherData.hourly.temperature2m[i]
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Get Weather</button>
     </div>
   );
 }
