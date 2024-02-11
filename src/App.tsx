c04e62d2b08ae20033e76278f392d9a966d3f832
Nicolas Crumrine <crumrinecoding@gmail.com>
2024-02-11 15:02:24 -0600

    WMO codes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,37 @@ import React, { useState } from "react";
 import { fetchWeatherApi } from "openmeteo";
 const url = "https://api.open-meteo.com/v1/forecast";
 
+const WMO_CODES = {
+  0: "Clear Sky",
+  1: "Mainly Clear",
+  2: "Partly Cloudy",
+  3: "Overcast",
+  45: "Fog",
+  48: "Depositing Rime Fog",
+  51: "Light drizzle",
+  53: "Moderate drizzle",
+  55: "Dense drizzle",
+  56: "Light freezing drizzle",
+  57: "Dense freezing drizzle",
+  61: "Slight rain",
+  63: "Moderate rain",
+  65: "Heavy rain",
+  66: "Light freezing rain",
+  67: "Heavy freezing rain",
+  71: "Slight snow fall",
+  73: "Moderate snow fall",
+  75: "Heavy snow fall",
+  77: "Snow grains",
+  80: "Slight rain showers",
+  81: "Moderate rain showers",
+  82: "Violent rain showers",
+  85: "Slight snow showers",
+  86: "Heavy snow showers",
+  95: "Thunderstorm",
+  96: "Thunderstorm with slight hail",
+  99: "Thunderstorm with heavy hail"
+}
+
 interface TodaysWeatherData {
   currentTemperature?: number;
   currentHumidity?: number;
@@ -241,6 +272,10 @@ function App() {
       return "#FF5337";
     }
   }
+
+  function convertWMO(code: number){
+
+  }
   return (
     <div
       className="App"
@@ -276,6 +311,8 @@ function App() {
       <h3>Tomorrow's Probability Max: {weeklyForecastData?.precipitationProbabilityMax?.[0] ?? "N/A"}</h3>
       <h3>Tomorrow's Wind Speed: {weeklyForecastData?.windspeed?.[0] ?? "N/A"}</h3>
       <h3>Tomorrow's Weather Code: {weeklyForecastData?.summary?.[0] ?? "N/A"}</h3>
+      <h3>Tomorrow's Weather Code: {weeklyForecastData?.summary?.[1] ?? "N/A"}</h3>
+      <h3>Tomorrow's Weather Code: {weeklyForecastData?.summary?.[2] ?? "N/A"}</h3>
     </div>
   );
 }
