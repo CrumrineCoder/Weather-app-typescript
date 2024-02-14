ca85f2d65ab408b32754233528cf47c9e69326f0
Nicolas Crumrine <crumrinecoding@gmail.com>
2024-02-13 20:02:47 -0600

    Testing getWeather

--- src/App.tsx
+++ src/App.tsx
@@ -182,121 +182,129 @@ function App() {
   function handleLocation(position: GeolocationPosition) {
     setLatitude(position.coords.latitude);
     setLongitude(position.coords.longitude);
-
-    getWeather(position.coords.latitude, position.coords.longitude);
+    //getWeather(latitude, longitude);
+     getWeather(position.coords.latitude, position.coords.longitude);
   }
 
-  async function getWeather(latitude: number, longitude: number) {
-    const params = {
-      latitude: latitude,
-      longitude: longitude,
-      current: [
-        "temperature_2m",
-        "relative_humidity_2m",
-        "apparent_temperature",
-        "precipitation",
-        "rain",
-        "showers",
-        "snowfall",
-        "wind_speed_10m",
-        "weather_code",
-      ],
-      daily: [
-        "temperature_2m_max",
-        "temperature_2m_min",
-        "apparent_temperature_max",
-        "apparent_temperature_min",
-        "sunrise",
-        "sunset",
-        "precipitation_sum",
-        "rain_sum",
-        "showers_sum",
-        "snowfall_sum",
-        "precipitation_hours",
-        "precipitation_probability_max",
-        "wind_speed_10m_max",
-        "weather_code",
-      ],
-      temperature_unit: "fahrenheit",
-      wind_speed_unit: "ms",
-      timezone: "MST",
-    };
+  async function getWeather(
+    latitude: number | undefined,
+    longitude: number | undefined
+  ) {
+    if (latitude && longitude) {
+      const params = {
+        latitude: latitude,
+        longitude: longitude,
+        current: [
+          "temperature_2m",
+          "relative_humidity_2m",
+          "apparent_temperature",
+          "precipitation",
+          "rain",
+          "showers",
+          "snowfall",
+          "wind_speed_10m",
+          "weather_code",
+        ],
+        daily: [
+          "temperature_2m_max",
+          "temperature_2m_min",
+          "apparent_temperature_max",
+          "apparent_temperature_min",
+          "sunrise",
+          "sunset",
+          "precipitation_sum",
+          "rain_sum",
+          "showers_sum",
+          "snowfall_sum",
+          "precipitation_hours",
+          "precipitation_probability_max",
+          "wind_speed_10m_max",
+          "weather_code",
+        ],
+        temperature_unit: "fahrenheit",
+        wind_speed_unit: "ms",
+        timezone: "MST",
+      };
 
-    const responses = await fetchWeatherApi(url, params);
+      const responses = await fetchWeatherApi(url, params);
 
-    const range = (start: number, stop: number, step: number) =>
-      Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+      const range = (start: number, stop: number, step: number) =>
+        Array.from(
+          { length: (stop - start) / step },
+          (_, i) => start + i * step
+        );
 
-    const response = responses[0];
+      const response = responses[0];
 
-    const utcOffsetSeconds = response.utcOffsetSeconds();
-    const current = response.current()!;
-    const daily = response.daily()!;
+      const utcOffsetSeconds = response.utcOffsetSeconds();
+      const current = response.current()!;
+      const daily = response.daily()!;
 
-    const weatherData = {
-      current: {
-        time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
-        temperature2m: current.variables(0)!.value(),
-        relativeHumidity2m: current.variables(1)!.value(),
-        apparentTemperature: current.variables(2)!.value(),
-        precipitation: current.variables(3)!.value(),
-        rain: current.variables(4)!.value(),
-        showers: current.variables(5)!.value(),
-        snowfall: current.variables(6)!.value(),
-        windSpeed10m: current.variables(7)!.value(),
-        weather_code: daily.variables(8)!.valuesArray()!,
-      },
-      daily: {
-        time: range(
-          Number(daily.time()),
-          Number(daily.timeEnd()),
-          daily.interval()
-        ).map((t) => new Date((t + utcOffsetSeconds) * 1000)),
-        temperature2mMax: daily.variables(0)!.valuesArray()!,
-        temperature2mMin: daily.variables(1)!.valuesArray()!,
-        apparentTemperatureMax: daily.variables(2)!.valuesArray()!,
-        apparentTemperatureMin: daily.variables(3)!.valuesArray()!,
-        sunrise: daily.variables(4)!.valuesArray()!,
-        sunset: daily.variables(5)!.valuesArray()!,
-        precipitationSum: daily.variables(6)!.valuesArray()!,
-        rainSum: daily.variables(7)!.valuesArray()!,
-        showersSum: daily.variables(8)!.valuesArray()!,
-        snowfallSum: daily.variables(9)!.valuesArray()!,
-        precipitationHours: daily.variables(10)!.valuesArray()!,
-        precipitationProbabilityMax: daily.variables(11)!.valuesArray()!,
-        windSpeed10mMax: daily.variables(12)!.valuesArray()!,
-        weather_code: daily.variables(13)!.valuesArray()!,
-      },
-    };
+      const weatherData = {
+        current: {
+          time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
+          temperature2m: current.variables(0)!.value(),
+          relativeHumidity2m: current.variables(1)!.value(),
+          apparentTemperature: current.variables(2)!.value(),
+          precipitation: current.variables(3)!.value(),
+          rain: current.variables(4)!.value(),
+          showers: current.variables(5)!.value(),
+          snowfall: current.variables(6)!.value(),
+          windSpeed10m: current.variables(7)!.value(),
+          weather_code: daily.variables(8)!.valuesArray()!,
+        },
+        daily: {
+          time: range(
+            Number(daily.time()),
+            Number(daily.timeEnd()),
+            daily.interval()
+          ).map((t) => new Date((t + utcOffsetSeconds) * 1000)),
+          temperature2mMax: daily.variables(0)!.valuesArray()!,
+          temperature2mMin: daily.variables(1)!.valuesArray()!,
+          apparentTemperatureMax: daily.variables(2)!.valuesArray()!,
+          apparentTemperatureMin: daily.variables(3)!.valuesArray()!,
+          sunrise: daily.variables(4)!.valuesArray()!,
+          sunset: daily.variables(5)!.valuesArray()!,
+          precipitationSum: daily.variables(6)!.valuesArray()!,
+          rainSum: daily.variables(7)!.valuesArray()!,
+          showersSum: daily.variables(8)!.valuesArray()!,
+          snowfallSum: daily.variables(9)!.valuesArray()!,
+          precipitationHours: daily.variables(10)!.valuesArray()!,
+          precipitationProbabilityMax: daily.variables(11)!.valuesArray()!,
+          windSpeed10mMax: daily.variables(12)!.valuesArray()!,
+          weather_code: daily.variables(13)!.valuesArray()!,
+        },
+      };
 
-    setTodaysWeatherData({
-      currentTemperature: weatherData.current.temperature2m,
-      currentHumidity: weatherData.current.relativeHumidity2m,
-      apparentTemperature: weatherData.current.apparentTemperature,
-      precipitation: weatherData.current.precipitation,
-      rain: weatherData.current.rain,
-      showers: weatherData.current.showers,
-      snowfall: weatherData.current.snowfall,
-      windSpeed: weatherData.current.windSpeed10m,
-      summary: weatherData.current.weather_code,
-    });
+      setTodaysWeatherData({
+        currentTemperature: weatherData.current.temperature2m,
+        currentHumidity: weatherData.current.relativeHumidity2m,
+        apparentTemperature: weatherData.current.apparentTemperature,
+        precipitation: weatherData.current.precipitation,
+        rain: weatherData.current.rain,
+        showers: weatherData.current.showers,
+        snowfall: weatherData.current.snowfall,
+        windSpeed: weatherData.current.windSpeed10m,
+        summary: weatherData.current.weather_code,
+      });
 
-    setWeeklyForecastData({
-      temperatureMax: weatherData.daily.temperature2mMax,
-      temperatureMin: weatherData.daily.temperature2mMin,
-      apparentTemperatureMax: weatherData.daily.apparentTemperatureMax,
-      apparentTemperatureMin: weatherData.daily.apparentTemperatureMin,
-      sunrise: weatherData.daily.sunrise,
-      sunset: weatherData.daily.sunset,
-      precipitationSum: weatherData.daily.precipitationSum,
-      rainSum: weatherData.daily.rainSum,
-      showersSum: weatherData.daily.showersSum,
-      precipitationHours: weatherData.daily.precipitationHours,
-      precipitationProbabilityMax:
-        weatherData.daily.precipitationProbabilityMax,
-      windspeed: weatherData.daily.windSpeed10mMax,
-      summary: weatherData.daily.weather_code,
-    });
+      setWeeklyForecastData({
+        temperatureMax: weatherData.daily.temperature2mMax,
+        temperatureMin: weatherData.daily.temperature2mMin,
+        apparentTemperatureMax: weatherData.daily.apparentTemperatureMax,
+        apparentTemperatureMin: weatherData.daily.apparentTemperatureMin,
+        sunrise: weatherData.daily.sunrise,
+        sunset: weatherData.daily.sunset,
+        precipitationSum: weatherData.daily.precipitationSum,
+        rainSum: weatherData.daily.rainSum,
+        showersSum: weatherData.daily.showersSum,
+        precipitationHours: weatherData.daily.precipitationHours,
+        precipitationProbabilityMax:
+          weatherData.daily.precipitationProbabilityMax,
+        windspeed: weatherData.daily.windSpeed10mMax,
+        summary: weatherData.daily.weather_code,
+      });
+    }
   }
 
   function getTemperatureId(temperature: number | undefined) {
